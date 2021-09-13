de38c49c7016e2161174997161e7629d3bd67341
alexmcdermid <alexander.mcdermid@gmail.com>
2021-09-13 09:06:59 -0700

    add shadow to winning text

--- script.js
+++ script.js
@@ -202,8 +202,6 @@ function render() {
     playerTurnEl.innerText = "Player Turn: "+playerTurn;
 }
 
-//todo dry
-// update: it is done
 // logic to deal with filling the correct item based on which column is pressed
 // makes sure that it is placing the disc at the top of the stack
 function update() {
@@ -225,120 +223,6 @@ function update() {
             }
         }
     }
-
-    // //  check that class position is empty
-    // //  far left column logic
-    //  if (currentClick === 0 ||currentClick % 7 === 0) {
-    //      if (boardArray[35] == 0) {
-    //          toFill = 35;
-    //      } else if (boardArray[35] != 0 && boardArray[28] == 0) {
-    //          toFill = 28;
-    //      } else if (boardArray[28] != 0 && boardArray[21] == 0) {
-    //          toFill = 21;
-    //      } else if (boardArray[21] != 0 && boardArray[14] == 0) {
-    //          toFill = 14;
-    //      } else if (boardArray[14] != 0 && boardArray[7] == 0) {
-    //          toFill = 7;
-    //      } else if (boardArray[7] != 0 && boardArray[0] == 0) {
-    //          toFill = 0;
-    //      }
-    //  }
-    //  //column 01 logic
-    //   if (currentClick === 1||currentClick % 7 === 1) {
-    //      if (boardArray[36] == 0) {
-    //          toFill = 36;
-    //      } else if (boardArray[36] != 0 && boardArray[29] == 0) {
-    //          toFill = 29;
-    //      } else if (boardArray[29] != 0 && boardArray[22] == 0) {
-    //          toFill = 22;
-    //      } else if (boardArray[22] != 0 && boardArray[15] == 0) {
-    //          toFill = 15;
-    //      } else if (boardArray[15] != 0 && boardArray[8] == 0) {
-    //          toFill = 8;
-    //      } else if (boardArray[8] != 0 && boardArray[1] == 0) {
-    //          toFill = 1;
-    //      }
-    //  }
-    // //column 02 logic
-    // if (currentClick === 2 || currentClick % 7 === 2) {
-    //     if (boardArray[37] == 0) {
-    //         toFill = 37;
-    //     } else if (boardArray[37] != 0 && boardArray[30] == 0) {
-    //         toFill = 30;
-    //     } else if (boardArray[30] != 0 && boardArray[23] == 0) {
-    //         toFill = 23;
-    //     } else if (boardArray[23] != 0 && boardArray[16] == 0) {
-    //         toFill = 16;
-    //     } else if (boardArray[16] != 0 && boardArray[9] == 0) {
-    //         toFill = 9;
-    //     } else if (boardArray[9] != 0 && boardArray[2] == 0) {
-    //         toFill = 2;
-    //     }
-    // }
-    // //column 03 logic
-    // if (currentClick === 3|| currentClick % 7 === 3) {
-    //     if (boardArray[38] == 0) {
-    //         toFill = 38;
-    //     } else if (boardArray[38] != 0 && boardArray[31] == 0) {
-    //         toFill = 31;
-    //     } else if (boardArray[31] != 0 && boardArray[24] == 0) {
-    //         toFill = 24;
-    //     } else if (boardArray[24] != 0 && boardArray[17] == 0) {
-    //         toFill = 17;
-    //     } else if (boardArray[17] != 0 && boardArray[10] == 0) {
-    //         toFill = 10;
-    //     } else if (boardArray[10] != 0 && boardArray[3] == 0) {
-    //         toFill = 3;
-    //     }
-    // }
-    //  //column 04 logic
-    //  if (currentClick === 4||currentClick % 7 === 4) {
-    //     if (boardArray[39] == 0) {
-    //         toFill = 39;
-    //     } else if (boardArray[39] != 0 && boardArray[32] == 0) {
-    //         toFill = 32;
-    //     } else if (boardArray[32] != 0 && boardArray[25] == 0) {
-    //         toFill = 25;
-    //     } else if (boardArray[25] != 0 && boardArray[18] == 0) {
-    //         toFill = 18;
-    //     } else if (boardArray[18] != 0 && boardArray[11] == 0) {
-    //         toFill = 11;
-    //     } else if (boardArray[11] != 0 && boardArray[4] == 0) {
-    //         toFill = 4;
-    //     }
-    // }
-    //   //column 05 logic
-    //   if (currentClick === 5||currentClick % 7 === 5) {
-    //     if (boardArray[40] == 0) {
-    //         toFill = 40;
-    //     } else if (boardArray[40] != 0 && boardArray[33] == 0) {
-    //         toFill = 33;
-    //     } else if (boardArray[33] != 0 && boardArray[26] == 0) {
-    //         toFill = 26;
-    //     } else if (boardArray[26] != 0 && boardArray[19] == 0) {
-    //         toFill = 19;
-    //     } else if (boardArray[19] != 0 && boardArray[12] == 0) {
-    //         toFill = 12;
-    //     } else if (boardArray[12] != 0 && boardArray[5] == 0) {
-    //         toFill = 5;
-    //     }
-    // }
-    //  //far right column logic
-    //  if (currentClick === 6||currentClick % 7 === 6) {
-    //     if (boardArray[41] == 0) {
-    //         toFill = 41;
-    //     } else if (boardArray[41] != 0 && boardArray[34] == 0) {
-    //         toFill = 34;
-    //     } else if (boardArray[34] != 0 && boardArray[27] == 0) {
-    //         toFill = 27;
-    //     } else if (boardArray[27] != 0 && boardArray[20] == 0) {
-    //         toFill = 20;
-    //     } else if (boardArray[20] != 0 && boardArray[13] == 0) {
-    //         toFill = 13;
-    //     } else if (boardArray[13] != 0 && boardArray[6] == 0) {
-    //         toFill = 6;
-    //     }
-    // }
 
     //adding filled items to winning array for each player
     if (playerTurn == 1&&!p1Array.includes(toFill)) {
